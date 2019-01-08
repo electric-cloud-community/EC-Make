d448d4927e7847ec2273f3f33d02260a95ab3162
Valentyn Solomko <Valentyn.Solomko@portaone.com>
2019-01-08 13:03:38 +0200

    Adapt for building with Gradle

--- src/main/resources/project/manifest.pl
+++
@@ -1,6 +0,0 @@
-@files = (
-    ['//property[propertyName="makeDriver"]/value', 'makeDriver.pl'],
-    ['//property[propertyName="postp_matchers"]/value', 'postp_matchers.pl'],
-    ['//procedure[procedureName="runMake"]/propertySheet/property[propertyName="ec_parameterForm"]/value', 'forms/runMake.xml'],
-    ['//property[propertyName="ec_setup"]/value', 'ec_setup.pl'],
-);
